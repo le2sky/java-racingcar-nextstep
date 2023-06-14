02c7da2ecb918a43f450fb7e261e8edf7361ef3f
leesky <leehaneul0623@gmail.com>
2023-06-14 21:51:38 +0900

    refactor : 구분자 상수 추출

--- src/main/java/racingcargame/controller/InputMapper.java
+++ src/main/java/racingcargame/controller/InputMapper.java
@@ -2,8 +2,10 @@ package racingcargame.controller;
 
 class InputMapper {
 
+    private static final String SPLIT_DELIMITER = ",";
+
     static String[] mapToNames(String input) {
-        return input.split(",");
+        return input.split(SPLIT_DELIMITER);
     }
 
     static int mapToRound(String input) {
