fc90e06c19c5840e66a1aba042d15abed51b0b8c
le2sky <leehaneul990623@gmail.com>
2023-06-30 09:42:03 +0900

    feat : 자동차 전진 기능 구현

--- src/main/java/racingcargamev2/domain/Car.java
+++ src/main/java/racingcargamev2/domain/Car.java
@@ -2,9 +2,36 @@ package racingcargamev2.domain;
 
 public class Car {
 
+    private static final int MIN_CONDITION_RANGE = 0;
+    private static final int MAX_CONDITION_RANGE = 9;
+
     private final Name name;
+    private final Position position;
 
     public Car(String name) {
         this.name = new Name(name);
+        this.position = Position.zero();
+    }
+
+    public void move(int condition) {
+        checkMoveConditionRange(condition);
+
+        if (condition >= 4) {
+            position.increase();
+        }
+    }
+
+    private void checkMoveConditionRange(int condition) {
+        if (isMoveConditionInRange(condition)) {
+            throw new IllegalArgumentException("전진에 필요한 숫자는 0에서 9사이의 숫자입니다.");
+        }
+    }
+
+    private boolean isMoveConditionInRange(int condition) {
+        return condition < MIN_CONDITION_RANGE || condition > MAX_CONDITION_RANGE;
+    }
+
+    public Position getPosition() {
+        return position;
     }
 }
--- src/test/java/racingcargamev2/domain/CarTest.java
+++ src/test/java/racingcargamev2/domain/CarTest.java
@@ -1,5 +1,6 @@
 package racingcargamev2.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
@@ -29,5 +30,30 @@ public class CarTest {
         assertThatThrownBy(() -> new Car(null))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("자동차 이름에 널값을 허용하지 않습니다.");
+    }
+
+    @DisplayName("자동차의 전진 조건은 4이상의 숫자이다.")
+    @Test
+    void move() {
+        Car car = new Car("2sky");
+
+        car.move(4);
+        car.move(3);
+
+        assertThat(car.getPosition()).isEqualTo(Position.with(1));
+    }
+
+    @DisplayName("전진에 필요한 숫자는 0에서 9사이의 숫자이다.")
+    @Test
+    void moveNumberRange() {
+        Car car = new Car("2sky");
+
+        assertThatThrownBy(() -> car.move(-1))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("전진에 필요한 숫자는 0에서 9사이의 숫자입니다.");
+
+        assertThatThrownBy(() -> car.move(10))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("전진에 필요한 숫자는 0에서 9사이의 숫자입니다.");
     }
 }
