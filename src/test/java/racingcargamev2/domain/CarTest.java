777f80b0e3c3b0558c49bcbb704744e2a5c74fa7
le2sky <leehaneul990623@gmail.com>
2023-07-02 01:42:53 +0900

    refactor : Name 테스트 내부로 Car 내부 이름 검증 테스트 이동

--- src/test/java/racingcargamev2/domain/CarTest.java
+++ src/test/java/racingcargamev2/domain/CarTest.java
@@ -8,30 +8,6 @@ import org.junit.jupiter.api.Test;
 
 public class CarTest {
 
-    @DisplayName("자동차 이름은 5자를 초과할 수 없다.")
-    @Test
-    void carNameLength() {
-        assertThatThrownBy(() -> new Car("123456"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("자동차 이름은 5자를 초과할 수 없습니다.");
-    }
-
-    @DisplayName("자동차 이름은 빈문자열이 될 수 없다.")
-    @Test
-    void carNameIsNotEmptyString() {
-        assertThatThrownBy(() -> new Car(""))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("자동차 이름은 빈 문자열이 될 수 없습니다.");
-    }
-
-    @DisplayName("자동차 이름에 널값을 허용하지 않는다.")
-    @Test
-    void carNameIsNotAllowNull() {
-        assertThatThrownBy(() -> new Car(null))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("자동차 이름에 널값을 허용하지 않습니다.");
-    }
-
     @DisplayName("자동차의 전진 조건은 4이상의 숫자이다.")
     @Test
     void move() {
